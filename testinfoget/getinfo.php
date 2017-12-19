2943e40860b4b868aecffba8ea34b0a070ccbdf4
maoyunyao <32135505+maoyunyao@users.noreply.github.com>
2017-12-19 22:15:47 +0800

    This is 1.0
    date 2017.12.19

---
+++ testinfoget/getinfo.php
@@ -0,0 +1,19 @@
+﻿<?php
+$user=$_GET["name"];
+
+$con = mysqli_connect('localhost', 'root', 'myy130805010017');
+if (!$con)
+ {
+ die('Could not connect: ' . mysqli_error());
+ }
+
+mysqli_select_db($con,"class");
+
+$sql="SELECT * FROM test_news where teammate = '$user'";
+
+$result = mysqli_query($con,$sql);
+$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+echo "<p>".$row["news"]."</p>";
+mysqli_free_result($result);
+mysqli_close($con);
+?>
